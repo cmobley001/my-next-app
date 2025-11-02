52931fcaeb04cd61b0cc47b4d2b8e8cde864ce58
cmobley001 <cmobley001@student.butte.edu>
2025-11-02 11:15:07 -0800

    setting up csci 31 lab

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,55 @@
-import Image from "next/image";
+import Main from "../components/Main";
+import Button from "../components/Button";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Card from "../components/Card";
+import Hero from "../components/Hero";
+import Content from "../components/Content";
+import Flex from "../components/Flex";
+// import LabWeek from "../../components/LabWeek";
 
-export default function Home() {
+const cards = [
+  {
+    title: "The Coldest Sunset",
+    img: "https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "The Warmest Sunset",
+    img: "https://images.unsplash.com/photo-1466618572466-002f11d518db?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "The Coolest Ocean",
+    img: "https://images.unsplash.com/photo-1468861389126-e0ca735dba0d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+const Page = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <>
+      <Navbar />
+      <div className="flex flex-col min-h-screen">
+        <div className="p-12 flex-1">
+          <Hero />
+          <Main>
+            <Flex classNames="justify-center gap-2.5">
+              <Button title="Click Me" />
+              <Button title="And Click Me" />
+            </Flex>
+            <div
+              className="flex gap-x-12 my-12 flex-wrap gap-y-6"
+              id="card-container"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              {cards.map((card, index) => (
+                <Card key={index} title={card.title} image={card.img} />
+              ))}
+            </div>
+          </Main>
+          <Content />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+        <Footer />
+      </div>
+    </>
   );
-}
+};
+
+export default Page;
---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="mt-auto w-full p-12 bg-indigo-50 text-center">
+      <div>❤️ ❤️ Created with love by CSCI 31 Students ❤️ ❤️</div>
+    </footer>
+  );
+}
+
+export default Footer;
